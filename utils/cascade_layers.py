658548d035f89f2afb5f016378d4d25627345ea3
Rimjhim0110 <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-13 13:49:15 +0530

    Update cascade_layers.py

--- utils/cascade_layers.py
+++ utils/cascade_layers.py
@@ -3,13 +3,13 @@ from typing import Any, Sequence
 
 def cascade_layers(layers: Sequence[tf.keras.layers.Layer], input_data: Any, is_training: bool = True) -> Any:
 
-    # Initialize the output variable with the input data.
+    # Initialize the output variable with the input data
     output_data = input_data
     
-    # Iterate through the layers in the sequence.
+    # Iterate through the layers in the sequence
     for layer in layers:
-        # Apply the current layer to the output data, specifying the training mode.
+        # Apply the current layer to the output data, specifying the training mode
         output_data = layer(output_data, training=is_training)
     
-    # Return the final output data after applying all layers in the cascade.
+    # Return the final output data after applying all layers in the cascade
     return output_data
