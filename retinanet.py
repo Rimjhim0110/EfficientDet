932b44482414989c0cb79092c5771e5b4b3753a5
Rimjhim Agarwal <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-14 00:09:46 +0530

    Update retinanet.py

--- retinanet.py
+++ retinanet.py
@@ -1,7 +1,7 @@
 import math
 import tensorflow as tf
 
-from . import layers
+from . import cnn_layers
 from efficientdet.utils import cascade_layers
 
 class RetinaNetBBPredictor(tf.keras.Model):
@@ -9,7 +9,7 @@ class RetinaNetBBPredictor(tf.keras.Model):
         super(RetinaNetBBPredictor, self).__init__()
         self.num_anchors = num_anchors
 
-        self.feature_extractors = [layers.ConvBlock(width, kernel_size=3, activation='swish', padding='same', separable=True, pointwise_initializer=tf.initializers.VarianceScaling(), depthwise_initializer=tf.initializers.VarianceScaling(), prefix=prefix + f'conv_block_{i}/')
+        self.feature_extractors = [cnn_layers.ConvBlock(width, kernel_size=3, activation='swish', padding='same', separable_conv=True, pointwise_initializer=tf.initializers.VarianceScaling(), depthwise_initializer=tf.initializers.VarianceScaling(), layer_prefix=prefix + f'conv_block_{i}/')
             for i in range(depth)]
 
         self.bb_regressor = tf.keras.layers.SeparableConv2D(num_anchors * 4, depth_multiplier=1, pointwise_initializer=tf.initializers.VarianceScaling(), depthwise_initializer=tf.initializers.VarianceScaling(), kernel_size=3, padding='same', name=prefix + 'regress_conv')
@@ -17,19 +17,18 @@ class RetinaNetBBPredictor(tf.keras.Model):
     def call(self, features: tf.Tensor, training: bool = True) -> tf.Tensor:
         batch_size = tf.shape(features)[0]
 
-        x = tf_utils.cascade_layers(self.feature_extractors, features, training=training)
+        x = cascade_layers.cascade_layers(self.feature_extractors, features, training=training)
         return tf.reshape(self.bb_regressor(x), [batch_size, -1, 4])
 
 
 class RetinaNetClassifier(tf.keras.Model):
-
     def __init__(self, width: int, depth: int, num_classes: int, num_anchors: int = 9, prefix: str = ''):
         super(RetinaNetClassifier, self).__init__()
         self.num_anchors = num_anchors
         self.num_classes = num_classes
 
         self.feature_extractors = [
-            layers.ConvBlock(width, kernel_size=3, activation='swish', padding='same', prefix=prefix + f'conv_block_{i}/', separable=True, pointwise_initializer=tf.initializers.VarianceScaling(), depthwise_initializer=tf.initializers.VarianceScaling(), bias_initializer='zeros')
+            cnn_layers.ConvBlock(width, kernel_size=3, activation='swish', padding='same', layer_prefix=prefix + f'conv_block_{i}/', separable_conv=True, pointwise_initializer=tf.initializers.VarianceScaling(), depthwise_initializer=tf.initializers.VarianceScaling(), bias_initializer='zeros')
             for i in range(depth)]
         
         prob = 0.01
