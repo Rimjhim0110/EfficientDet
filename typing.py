e885456963461339d3be11e50cfbf81001410659
git0014 <103322645+git0014@users.noreply.github.com>
2023-10-15 19:49:38 +0530

    Update typing.py

--- typing.py
+++ typing.py
@@ -1,7 +1,5 @@
-from typing import Tuple
-
+from typing import Tuple #typing module is used to type hints and annotations 
 import tensorflow as tf
-
 
 Annotation = Tuple[tf.Tensor, tf.Tensor]
 ObjectDetectionInstance = Tuple[tf.Tensor, Annotation]
