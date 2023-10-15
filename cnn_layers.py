6aebc143f834d4670218d480632de0f0e586361d
Rimjhim Agarwal <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-15 20:13:11 +0530

    Update cnn_layers.py

--- cnn_layers.py
+++ cnn_layers.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from typing import Any, Tuple, Sequence
+from config import AnchorsConfig
+from utils import bbox_utils, anchor_utils
 
 class ResizeLayer(tf.keras.layers.Layer):
     def __init__(self, num_features: int, prefix: str = '') -> None:
@@ -50,3 +52,41 @@ class ConvBlock(tf.keras.layers.Layer):
         output = self.activation(conv_output)
 
         return output
+
+
+class FilterDetections(object):
+    def __init__(self, anchors_config: AnchorsConfig, score_threshold: float):
+        self.score_threshold = score_threshold
+        self.anchors_gen = [anchor_utils.GenerateAnchor(base_size=anchors_config.sizes[i - 3], aspect_ratios=anchors_config.ratios, stride=anchors_config.strides[i - 3]
+                                                   ) for i in range(3, 8)] 
+        # 3 to 7 pyramid levels
+
+        # Accelerate calls
+        self.regress_boxes = tf.function(bbox_utils.regress_bboxes, input_signature=[
+                tf.TensorSpec(shape=[None, None, 4], dtype=tf.float32),
+                tf.TensorSpec(shape=[None, None, 4], dtype=tf.float32)])
+
+        self.clip_boxes = tf.function(bbox_utils.clip_boxes, input_signature=[
+                tf.TensorSpec(shape=[None, None, 4], dtype=tf.float32),
+                tf.TensorSpec(shape=None)])
+    
+    def __call__(self, images: tf.Tensor, regressors: tf.Tensor, class_scores: tf.Tensor) -> Tuple[Sequence[tf.Tensor], Sequence[tf.Tensor], Sequence[tf.Tensor]]:
+        im_shape = tf.shape(images)
+        batch_size, h, w = im_shape[0], im_shape[1], im_shape[2]
+
+        # Create the anchors
+        shapes = [w // (2 ** x) for x in range(3, 8)]
+        anchors = [g((size, size, 3)) for g, size in zip(self.anchors_gen, shapes)]
+        anchors = tf.concat(anchors, axis=0)
+        
+        # Tile anchors over batches, so they can be regressed
+        anchors = tf.tile(tf.expand_dims(anchors, 0), [batch_size, 1, 1])
+
+        # Regress anchors and clip in case they go outside of the image
+        boxes = self.regress_boxes(anchors, regressors)
+        boxes = self.clip_boxes(boxes, [h, w])
+
+        # Suppress overlapping detections
+        boxes, labels, scores = bbox_utils.nms(boxes, class_scores, score_threshold=self.score_threshold)
+
+        return boxes, labels, scores
