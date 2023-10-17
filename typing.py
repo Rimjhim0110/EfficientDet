fea1f9d5a127b82b43a95b011caa38367f575505
Rimjhim Agarwal <76653947+Rimjhim0110@users.noreply.github.com>
2023-10-17 21:23:05 +0530

    Delete typing.py

--- typing.py
+++
@@ -1,5 +0,0 @@
-from typing import Tuple #typing module is used to type hints and annotations 
-import tensorflow as tf
-
-Annotation = Tuple[tf.Tensor, tf.Tensor]
-ObjectDetectionInstance = Tuple[tf.Tensor, Annotation]
